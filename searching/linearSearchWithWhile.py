f28acebffe8d9125ce4243cdf64ac01b6f95ee63
Zoobdude <96008479+Zoobdude@users.noreply.github.com>
2023-01-13 11:01:12 +0000

    added check if it never finds

--- searching/linearSearchWithWhile.py
+++ searching/linearSearchWithWhile.py
@@ -4,10 +4,12 @@ searchName = input("Searched name ")
 foundName = False
 currentRecordNum = 0
 
-while foundName == False and currentRecordNum <= len(people):
+while foundName == False and currentRecordNum < len(people):
     if searchName == people[currentRecordNum]:
         foundName = True
+        print(f"Name: {searchName} was found in position {currentRecordNum + 1}")
     else:
         currentRecordNum += 1
 
-print(f"Name: {searchName} was found in position {currentRecordNum + 1}")
+if foundName == False:
+    print(f"Name: {searchName} was never found")
